deaeadcb180b39c38b935ad3ca821c27b46714b9
shane newsom <shaned@pm.me>
2025-06-16 19:15:00 -0500

    Add project files.

---
+++ GymTracker.Domain/Models/CardioSession.cs
@@ -0,0 +1,11 @@
+namespace GymTracker.Domain.Models;
+
+public class CardioSession
+{
+    public int Id { get; set; }
+    public DateTime DateTime { get; set; }
+    public TimeSpan Duration { get; set; }
+    public int CaloriesBurned { get; set; }
+    public int CardioTypeId { get; set; }
+    public int LocationId { get; set; }
+} 
---
+++ GymTracker.Domain/Models/WeightTrainingSession.cs
@@ -0,0 +1,11 @@
+namespace GymTracker.Domain.Models;
+
+public class WeightTrainingSession
+{
+    public int Id { get; set; }
+    public DateTime DateTime { get; set; }
+    public int Reps { get; set; }
+    public decimal Weight { get; set; }
+    public int WorkoutTypeId { get; set; }
+    public int LocationId { get; set; }
+} 
